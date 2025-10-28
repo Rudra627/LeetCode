4bbe9ebd9f1060de9fe8565b6ed96a58fa93e0f0
Rudra627 <rudrasahu9007@gmail.com>
2025-10-28 10:53:00 +0530

    Create LeetCode242.java

---
+++ LEETCODE/LeetCode242.java
@@ -0,0 +1,13 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        int n=s.length();
+        int m=t.length();
+        if(m!=n) return false;
+        char arr1[]=s.toCharArray();
+        char arr2[]=t.toCharArray();
+        Arrays.sort(arr1);
+        Arrays.sort(arr2);
+
+        return Arrays.equals(arr1, arr2);
+    }
+}
